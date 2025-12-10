19a966841e7480d3359fa4b3b9e8344ba53a704f
Garv Goel <goelgarv99@gmail.com>
2025-12-10 16:44:22 +0530

    adding res cards

--- app.js
+++ app.js
@@ -58,11 +58,44 @@ const Header = () => {
     </div>
   );
 };
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/37e31a5f-6dc1-4ef2-8a9f-67d075111a4e_16865.JPG"
+      />
+      <h3>Pizza Hut </h3>
+      <h4>Burger, Pizza, Fries</h4>
+      <h4>5 star</h4>
+      <h4>30mins</h4>
+    </div>
+  );
+};
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
