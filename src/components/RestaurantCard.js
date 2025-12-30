1cc665fa86d054ab7624b18f1c26b34f31e5852a
Garv Goel <goelgarv99@gmail.com>
2025-12-30 14:23:15 +0530

    top rated rest

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -42,7 +42,7 @@ const RestaurantCard = ({ resData }) => {
       <img className="res-logo" src={CDN_URL + cloudinaryImageId} alt={name} />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>⭐ {avgRating}</h4>
+      <h4> {avgRating}</h4>
       <h4>{sla.deliveryTime} mins</h4>
     </div>
   );
