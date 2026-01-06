8dfaffce055e32a8139bd5debf820e84a8d0ee7d
Garv Goel <goelgarv99@gmail.com>
2026-01-06 19:47:44 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,3 @@
-/**import RestaurantCard from "./RestaurantCard";
-import reslist from "../utils/mockdata";
-const Body = () => {
-  return (
-    <div className="body">
-      <div className="filter">
-        <button className="filter-btn" onClick={() => {}}>
-          Top Rated Restaurant
-        </button>
-      </div>
-      <div className="res-container">
-        {reslist.map((restaurant, index) => (
-          <RestaurantCard key={index} resData={restaurant} />
-        ))}
-      </div>
-    </div>
-  );
-};
-export default Body;
-**/
 import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import reslist from "../utils/mockdata";
