9ecfdd268392313f82ac14c7b8bbb78f420a7d2a
Garv Goel <goelgarv99@gmail.com>
2026-01-07 01:15:51 +0530

    fetch data from live api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import reslist from "../utils/mockdata";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(reslist);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      " https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
   return (
     <div className="body">
       <div className="filter">
