65e133958fd34df129c078a0f3ba9c960f3a773a
Garv Goel <goelgarv99@gmail.com>
2026-01-12 15:16:40 +0530

    usestate in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const User = (props) => {
+  const [count] = useState(0);
   return (
     <div className="user-card">
+      <h1>count ={count}</h1>
       <h2>{props.name}</h2>
       <h3>Delhi</h3>
       <h4>@garvgoel13</h4>
