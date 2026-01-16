df91e88115825872aa0a7bb189265f1530849f79
Garv Goel <goelgarv99@gmail.com>
2026-01-17 00:19:07 +0530

    logo fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL =
-  //"https://thumbs.dreamstime.com/b/food-delivery-logo-design-template-134749604.jpg";
-  "https://cdn-icons-png.flaticon.com/128/1532/1532688.png";
+  "https://thumbs.dreamstime.com/b/food-delivery-logo-design-template-134749604.jpg";
+//"https://cdn-icons-png.flaticon.com/128/1532/1532688.png";
 
 export const MENU_API =
   "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
