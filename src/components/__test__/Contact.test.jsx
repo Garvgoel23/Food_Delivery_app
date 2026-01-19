56eeb556c7c6970ba78165b836bbf820f19d9583
Garv Goel <goelgarv99@gmail.com>
2026-01-19 17:28:35 +0530

    it

--- src/components/__test__/Contact.test.jsx
+++ src/components/__test__/Contact.test.jsx
@@ -1,32 +1,38 @@
 import Contact from "../Contact";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-test("Contact component renders correctly", () => {
-  render(<Contact />);
+import { describe, it } from "@jest/globals";
 
-  const heading = screen.getByRole("heading");
+describe("Contact Component Test Case", () => {
+  it("Contact component renders correctly", () => {
+    render(<Contact />);
 
-  expect(heading).toBeInTheDocument();
-});
-test("Contact component renders button correctly", () => {
-  render(<Contact />);
+    const heading = screen.getByRole("heading");
 
-  //const button = screen.getByRole("button");
-  const button = screen.getByText("Submit");
+    expect(heading).toBeInTheDocument();
+  });
+  it("Contact component renders button correctly", () => {
+    render(<Contact />);
 
-  expect(button).toBeInTheDocument();
-});
-test("Contact component renders button correctly", () => {
-  render(<Contact />);
+    //const button = screen.getByRole("button");
+    const button = screen.getByText("Submit");
 
-  //const button = screen.getByRole("button");
-  const input = screen.getByPlaceholderText("Enter your name");
+    expect(button).toBeInTheDocument();
+  });
+  it("Contact component renders input correctly", () => {
+    render(<Contact />);
 
-  expect(input).toBeInTheDocument();
-});
-test("should load 2 input boxes", () => {
-  render(<Contact />);
-  const inputboxes = screen.getAllByRole("textbox");
+    //const button = screen.getByRole("button");
+    const input = screen.getByPlaceholderText("Enter your name");
 
-  expect(inputboxes).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+  });
+
+  // it and test are one and the same thing
+  it("should load 2 input boxes", () => {
+    render(<Contact />);
+    const inputboxes = screen.getAllByRole("textbox");
+
+    expect(inputboxes.length).not.toBe(2);
+  });
 });
