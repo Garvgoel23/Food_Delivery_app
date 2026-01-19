62b5662645d7430b91afcddeebd24a98b8cc1eb2
Garv Goel <goelgarv99@gmail.com>
2026-01-19 16:35:07 +0530

    unit testing contact us page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,24 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact us page</h1>
+      <h1 className="font-bold text-3xl p-4 m-4 justify-center text-center">
+        Contact us page
+      </h1>
+      <form className="flex flex-col p-4 m-4">
+        <label className="font-medium text-lg mb-2" htmlFor="name">
+          Name:
+        </label>
+        <input
+          className="border border-gray-300 p-2 mb-4 rounded justify-center text-center"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+        />
+        <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
