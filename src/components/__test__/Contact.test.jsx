7ede334e411e101145a4c2c27e6e3c692ef476be
Garv Goel <goelgarv99@gmail.com>
2026-01-19 17:05:25 +0530

    testing contact us page

--- src/components/__test__/Contact.test.jsx
+++ src/components/__test__/Contact.test.jsx
@@ -8,3 +8,19 @@ test("Contact component renders correctly", () => {
 
   expect(heading).toBeInTheDocument();
 });
+test("Contact component renders button correctly", () => {
+  render(<Contact />);
+
+  //const button = screen.getByRole("button");
+  const button = screen.getByText("Submit");
+
+  expect(button).toBeInTheDocument();
+});
+test("Contact component renders button correctly", () => {
+  render(<Contact />);
+
+  //const button = screen.getByRole("button");
+  const input = screen.getByPlaceholderText("Enter your name");
+
+  expect(input).toBeInTheDocument();
+});
